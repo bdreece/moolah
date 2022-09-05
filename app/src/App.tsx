3073870f6e8bf94e3d97343b0908ddac5409603d
Brian Reece <bdreece@mtu.edu>
2022-09-05 16:02:13 -0400

    Added AGPLv3 license text

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,23 @@
+/**
+ * moolah - A simple budgeting app
+ * Copyright (C) 2022 Brian Reece
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import type { FC } from 'react';
 
-const App: FC = () => (
-    <div className="App">
-    </div>
-);
+const App: FC = () => <div className="App"></div>;
 
-export default App
+export default App;
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,18 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+/**
+ * moolah - A simple budgeting app
+ * Copyright (C) 2022 Brian Reece
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-import App from './App'
+import App from './App';
 
 const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-        <QueryClientProvider client={client}>
-            <App />
-        </QueryClientProvider>
+      <QueryClientProvider client={client}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
